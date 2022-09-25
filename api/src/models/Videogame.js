b5f5a54825319b3f4e7c89b3f5bfd92f7cb8cf5f
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-24 21:21:06 -0300

    first commit

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING,
+      // type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
+
+// ID: * No puede ser un ID de un videojuego ya existente en la API rawg
+// Nombre *
+// Descripción * !!!!
+// Fecha de lanzamiento
+// Rating
+// Plataformas *
