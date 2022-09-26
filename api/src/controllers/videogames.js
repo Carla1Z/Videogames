3dff810e4276b2a9846c1ccdccac38e9b48151c9
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-26 17:04:19 -0300

    videogames

--- api/src/controllers/videogames.js
+++ api/src/controllers/videogames.js
@@ -6,20 +6,24 @@ let url = `https://api.rawg.io/api/games?key=${API_KEY}`;
 
 const videogames = async () => {
   let videogamesApi = [];
-  for (let i = 1; i < 6; i++) {
+  for (let i = 0; i < 5; i++) {
     let apiData = await axios.get(url);
-    // console.log(apiData.data.results);
-    videogamesApi = videogamesApi.concat(apiData.data.results);
-    // console.log(videogamesApi);
-  }
 
-  videogamesApi = videogamesApi.map((info) => {
-    return{
-        id: info.id,
-        name: info.name,
-    }
-  })
-  console.log(videogamesApi.length);
+    videogamesApi = videogamesApi.concat(apiData.data.results);
+
+    videogamesApi = videogamesApi.map((game) => {
+      return {
+        id: game.id,
+        name: game.name,
+      };
+    });
+    url = apiData.data.next;
+    // console.log(url);
+    // console.log(videogamesApi);
+    // console.log(videogamesApi.length);
+  }
+  return videogamesApi;
 };
+
 
 module.exports = videogames;
--- api/src/routes/videogamesRouter.js
+++ api/src/routes/videogamesRouter.js
@@ -1,11 +1,27 @@
 const { Router } = require("express");
 const videogames = require("../controllers/videogames");
 
+
 const videogamesRouter = Router();
 
 videogamesRouter.get("", async (req, res) => {
-    console.log(videogames());
-  res.send("GET de la ruta videogames");
+  const { name } = req.query
+  const apiInfo = await videogames()
+  // console.log(apiInfo.length);
+
+  try {
+    if(name){
+      const videogameName = await apiInfo.filter((el) => el.name.toLowerCase().includes(name.toLowerCase()))
+
+      videogameName.length ? res.status(200).send(videogameName) : res.status(404).send("Videogame not found")
+    }else{
+      res.status(200).send(apiInfo)
+    }
+  } catch (error) {
+    console.log("Error en la ruta videogames= " + error);
+  }
+  //   console.log(videogames());
+  // res.send("GET de la ruta videogames");
 });
 
 videogamesRouter.get("/:id", async (req, res) => {
