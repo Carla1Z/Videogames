03b634ca562282354e6eccdef0ae6f506017e407
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-27 14:47:17 -0300

    paginado

--- client/src/components/Videogames/index.jsx
+++ client/src/components/Videogames/index.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getVideogames } from "../../redux/actions";
 import Card from "../Card";
+import { useState } from "react";
 
 function Videogames() {
   const dispatch = useDispatch();
@@ -12,22 +13,39 @@ function Videogames() {
     dispatch(getVideogames());
   }, [dispatch]);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const filteredVideogames = allVideogames.slice(currentPage, currentPage + 5);
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 5);
+  };
+
+  const prevPage = () => {
+    if (currentPage > 0) setCurrentPage(currentPage - 5);
+  };
+
   return (
-    <div className={styles.cards}>
-      {allVideogames ? (
-        allVideogames.map((game) => {
-          return (
-            <Card
-              key={game.id}
-              image={game.image}
-              name={game.name}
-              genres={game.genres}
-            />
-          );
-        })
-      ) : (
-        <h3>No se encontraron videojuegos</h3>
-      )}
+    <div>
+      <button onClick={prevPage}>Anterior</button>
+      <br />
+      <button onClick={nextPage}>Siguiente</button>
+
+      <div className={styles.cards}>
+        {filteredVideogames ? (
+          filteredVideogames.map((game) => {
+            return (
+              <Card
+                key={game.id}
+                image={game.image}
+                name={game.name}
+                genres={game.genres}
+              />
+            );
+          })
+        ) : (
+          <h3>No se encontraron videojuegos</h3>
+        )}
+      </div>
     </div>
   );
 }
