5f0e3c53678f5a6c8301f79314ebacbd1fe6f52b
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-28 20:54:15 -0300

    cards

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,13 +1,17 @@
-import styles from "./Card.module.css"
+import styles from "./Card.module.css";
 
-function Card({image, name, genres}){
-    return(
-        <div className={styles.card}>
-            <h3>{name}</h3>
-            <img src={image} alt="videogame" />
-            <p>{genres}</p>
+function Card({ image, name, genres }) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.fondo}>
+        <img src={image} alt="videogame" className={styles.image} />
+        <div className={styles.content}>
+          <h3>{name}</h3>
+          <p>{genres}</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Card;
