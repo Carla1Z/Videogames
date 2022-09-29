a36bc06279192423c73c200dcc04ff9a4a9be55c
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-29 15:24:34 -0300

    ordenamiento redux

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,12 +1,19 @@
-import axios from 'axios'
-import { GET_VIDEOGAMES } from './type'
+import axios from "axios";
+import { GET_ORDER_ABC, GET_VIDEOGAMES } from "./type";
 
-export function getVideogames(){
-    return async(dispatch) => {
-        let allVideogames = await axios.get('http://localhost:3001/videogames')
-        return dispatch({
-            type: GET_VIDEOGAMES,
-            payload: allVideogames.data,
-        })
-    }
+export function getVideogames() {
+  return async (dispatch) => {
+    let allVideogames = await axios.get("http://localhost:3001/videogames");
+    return dispatch({
+      type: GET_VIDEOGAMES,
+      payload: allVideogames.data,
+    });
+  };
+}
+
+export function getOrderAbc(payload) {
+  return {
+    type: GET_ORDER_ABC,
+    payload,
+  };
 }
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,7 +1,8 @@
-import { GET_VIDEOGAMES } from "./type";
+import { GET_ORDER_ABC, GET_VIDEOGAMES } from "./type";
 
 const initialState = {
   videogames: [],
+  totalVideogames: [],
 };
 
 export default function (state = initialState, action) {
@@ -10,8 +11,27 @@ export default function (state = initialState, action) {
       return {
         ...state,
         videogames: action.payload,
+        totalVideogames: action.payload,
       };
+    case GET_ORDER_ABC:
+      let sortVideogames = [...state.videogames];
 
+      sortVideogames =
+        action.payload === "asc"
+          ? state.videogames.sort(function (a, z) {
+              if (a.name > z.name) return 1;
+              if (a.name < z.name) return -1;
+              return 0;
+            })
+          : state.videogames.sort(function (a, z) {
+              if (a.name < z.name) return 1;
+              if (a.name > z.name) return -1;
+              return 0;
+            });
+      return {
+        ...state,
+        videogames: sortVideogames,
+      };
     default:
       return state;
   }
