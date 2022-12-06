7b1d39b6c9efa8bf543b141cd3eb60d80b74af4a
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-12-06 00:04:40 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,8 @@ import { store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://videogames-iyg8.onrender.com";
 
 ReactDOM.render(
   <Provider store={store}>
